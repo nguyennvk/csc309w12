0f5f3ad37784fd28f83920ccdc8f996b12e726f8
nguyennvk <nguyennvk@ghn.vn>
2025-04-01 10:46:38 -0400

    add

---
+++ e12/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div id="auto-tester">
+      Hi. I am nguy3369, and I have learned how to develop a full-stack web app
+      and deploy it!
+    </div>
+  )
+}
